9893c0fb381c804bf83e8124130116fdcf01bae5
Taufan FP Ruslanali <tfpruslanali@gmail.com>
2024-05-08 19:44:35 +0700

    feat: inject factory & singleton of Exercise list

--- momease/lib/injection_container.dart
+++ momease/lib/injection_container.dart
@@ -3,6 +3,11 @@ import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:momease/src/core/database/database.dart';
 import 'package:momease/src/core/navigation_bar/cubit/custom_navigation_bar_cubit.dart';
 import 'package:momease/src/core/network/network_info.dart';
+import 'package:momease/src/features/exercise/data/datasources/exercise_local_data_source.dart';
+import 'package:momease/src/features/exercise/data/repositories/exercise_repository_impl.dart';
+import 'package:momease/src/features/exercise/domain/repositories/exercise_repository.dart';
+import 'package:momease/src/features/exercise/domain/usecases/get_exercise_list.dart';
+import 'package:momease/src/features/exercise/presentation/bloc/exercise_bloc.dart';
 import 'package:momease/src/features/therapy/data/datasources/therapy_local_data_source.dart';
 import 'package:momease/src/features/therapy/data/repositories/therapy_repository_impl.dart';
 import 'package:momease/src/features/therapy/domain/repositories/therapy_repository.dart';
@@ -12,15 +17,19 @@ import 'package:momease/src/features/therapy/presentation/bloc/therapy_bloc.dart
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  // Features - Therapy
+  // Features
   sl.registerFactory(
     () => TherapyBloc(
       getTherapyList: sl(),
     ),
   );
+  sl.registerFactory(
+    () => ExerciseBloc(getExerciseList: sl()),
+  );
 
   // Use cases
   sl.registerLazySingleton(() => GetTherapyList(sl()));
+  sl.registerLazySingleton(() => GetExerciseList(sl()));
 
   // Repository
   sl.registerLazySingleton<TherapyRepository>(
@@ -28,10 +37,17 @@ Future<void> init() async {
       localDataSource: sl(),
     ),
   );
+  sl.registerLazySingleton<ExerciseRepository>(
+      () => ExerciseRepositoryImpl(localDataSource: sl()));
 
   // Data sources
   sl.registerLazySingleton<TherapyLocalDataSource>(
     () => TherapyLocalDataSourceImpl(
+      database: sl(),
+    ),
+  );
+  sl.registerLazySingleton<ExerciseLocalDataSource>(
+    () => ExerciseLocalDataSourceImpl(
       database: sl(),
     ),
   );
